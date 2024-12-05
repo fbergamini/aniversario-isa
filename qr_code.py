812802686516fe312da490ba6dcd21c45d2149b5
Fabio Bergamini Leonardo <fabio.bergamini@hotmail.com>
2024-12-05 13:14:20 -0300

    final

--- qr_code.py
+++ qr_code.py
@@ -1,18 +1,16 @@
 import os
-from qrcode import QRCode
 from dotenv import load_dotenv
+from qrcode import QRCode
 
 load_dotenv(override=True)
 BASE_URL = os.environ.get("BASE_URL")
-
 
 def gerar_qr_code(lado_camiseta: str):
     qr = QRCode()
     qr.add_data(f"{BASE_URL}/{lado_camiseta}.html")
     qr.make()
-    img = qr.make_image(fill_color="white", back_color="black")
+    img = qr.make_image()
     return img
-
 
 lado_camiseta = "frente"
 qr_code = gerar_qr_code(lado_camiseta)
