117e19ba19d129dca46e92e6159b2bc6e87f4156
Fabio Bergamini Leonardo <fabio.bergamini@hotmail.com>
2024-12-04 22:18:39 -0300

    first version

--- qr_code.py
+++ qr_code.py
@@ -2,20 +2,18 @@ import os
 from qrcode import QRCode
 from dotenv import load_dotenv
 
+load_dotenv(override=True)
+BASE_URL = os.environ.get("BASE_URL")
+
+
 def generate_qr_code(data):
     qr = QRCode()
     qr.add_data(data)
     qr.make()
-    img = qr.make_image(
-        fill_color="white",
-        back_color="black"
-    )
+    img = qr.make_image(fill_color="white", back_color="black")
     return img
 
-if __name__ == "__main__":
-    load_dotenv(override=True)
-    BASE_URL = os.environ.get("BASE_URL")
-    qr_code = generate_qr_code(
-        f"{BASE_URL}/fantasia.html"
-    )
-    qr_code.show()
+
+tshirt_side = "back"
+qr_code = generate_qr_code(f"{BASE_URL}/{tshirt_side}.html")
+qr_code.show()
